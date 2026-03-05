bc6c5206c1487f6621506b802ea28e25a3980670
RadoDrakonchik <dashamaxim23@gmail.com>
2026-03-05 22:15:48 +0500

    бла балбал

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: 'Homework8', // <-- ЗАМЕНИ НА НАЗВАНИЕ ТВОЕГО РЕПОЗИТОРИЯ
+  base: '/Homework8/', 
 })
